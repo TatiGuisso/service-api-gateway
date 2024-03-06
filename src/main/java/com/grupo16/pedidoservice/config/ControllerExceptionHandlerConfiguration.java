0554d9d2782bafe488382a0aba27db261cb33ef5
TatiGuisso <tati.guisso@gmail.com>
2024-03-05 22:13:45 -0300

    Adicionando controller exception handler

---
+++ src/main/java/com/grupo16/pedidoservice/config/ControllerExceptionHandlerConfiguration.java
@@ -0,0 +1,40 @@
+package com.grupo16.pedidoservice.config;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.grupo16.pedidoservice.exception.ExceptionJson;
+import com.grupo16.pedidoservice.exception.SystemBaseException;
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+@ControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class ControllerExceptionHandlerConfiguration {
+
+	@ExceptionHandler({ SystemBaseException.class})
+	@ResponseBody
+	public ResponseEntity<ExceptionJson> sasException(final SystemBaseException e, final HttpServletResponse response) {
+		final ExceptionJson exceptionJson = new ExceptionJson(e);
+		return new ResponseEntity<>(exceptionJson, new HttpHeaders(), e.getHttpStatus());
+	}
+
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	@ExceptionHandler(Throwable.class)
+	@ResponseBody
+	public ExceptionJson genericError(final Throwable e) {
+		log.error(e.getMessage(), e);
+		return new ExceptionJson(HttpStatus.INTERNAL_SERVER_ERROR.name(), e.getMessage());
+	}
+
+}
